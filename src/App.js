7ac8594104b1fcf6f344274390ffce035dae8c33
thyeone <thyeonee@gmail.com>
2022-11-08 01:34:39 +0900

    refactor: 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Login from "./pages/Login";
 import MyPage from "./pages/Mypage";
-import Board from "./components/Board";
 import GlobalStyle from "./GlobalStyle";
 import Register from "./pages/Register";
 import FindPw from "./components/FindPw";
 import Header from "./components/Header";
 import EditInfo from "./components/EditInfo";
+import Main from "./pages/Main";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Header />
-        <Routes>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/mypage" element={<MyPage />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/findPw" element={<FindPw />}></Route>
-          <Route path="/mypage/editinfo" element={<EditInfo />}></Route>
-          <Route path="/board" element={<Board />}></Route>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/main" element={<Main />}></Route>
+        <Route path="/mypage" element={<MyPage />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/findPw" element={<FindPw />}></Route>
+        <Route path="/mypage/editinfo" element={<EditInfo />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
