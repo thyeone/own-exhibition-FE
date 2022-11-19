383f70310fad04ed7c8c921764b4e706738cda27
thyeone <thyeonee@gmail.com>
2022-11-19 15:53:26 +0900

    feat: Main 배경, 문구 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,64 @@
 import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import background from "../assets/background.jpg";
 
-const Container = styled.div``;
+function Main() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (localStorage.getItem("token") == null) {
+      navigate("/");
+    }
+  }, []);
+
+  return (
+    <Container>
+      <VisualBox>
+        <img src={background} />
+        <h3 className="visualText">
+          문구
+          <br />
+          너만의 전시회
+        </h3>
+      </VisualBox>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  padding-top: 120px;
+`;
+const VisualBox = styled.section`
+  height: 772px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  img {
+    position: absolute;
+    width: 100vw;
+    height: 890px;
+    top: 0;
+    z-index: -10;
+    background-position: center;
+    background-size: cover;
+
+    /* position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: -2; */
+  }
+  h3 {
+    padding-top: 227px;
+    font-weight: bold;
+    font-size: 56px;
+    line-height: 70px;
+    text-align: center;
+    color: white;
+  }
+`;
 
 const Header = styled.div`
   position: fixed;
@@ -37,15 +93,5 @@ const List = styled.div`
   box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.25);
   0 8px 16px -8px rgba(0, 0, 0, 0.3), 0 -6px 16px -6px rgba(0, 0, 0, 0.025); */
 `;
-
-function Main() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (localStorage.getItem("token") == null) {
-      navigate("/");
-    }
-  }, []);
-  return <Container></Container>;
-}
 
 export default Main;
