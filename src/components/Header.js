94bf6123fbeb652b84647e4fea9d3253efbe74d3
thyeone <thyeonee@gmail.com>
2022-11-19 15:49:33 +0900

    refactor: Header fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,41 @@
+import axios from "axios";
 import { Link, useMatch, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import logo2 from "../assets/logo2.png";
-import Logout from "./shared/Logout";
 import mypage from "../assets/mypage_icon.png";
+import Logout from "./shared/Logout";
 
 function Header() {
   const loginMatch = useMatch("/");
   const registerMatch = useMatch("/register");
   const findPwMatch = useMatch("/findpw");
   const navigate = useNavigate();
+  const accessToken = localStorage.getItem("token");
+
+  // const onLogout = () => {
+  //   localStorage.clear();
+  //   navigate("/");
+  //   axios
+  //     .post("http://13.125.82.62/api/logout/", {
+  //       headers: {
+  //         Authorization: `Bearer ${accessToken}`,
+  //       },
+  //     })
+  //     .then((res) => {
+  //       console.log(res);
+  //       navigate("/");
+  //     })
+  //     .catch((error) => {
+  //       console.log("로그아웃 실패", error);
+  //     });
+  // };
+
   return loginMatch !== null ||
     registerMatch !== null ||
     findPwMatch !== null ? null : (
     <StyledHeader>
       <Link to="/main">
         <StyledLogo>
-          <img src={logo2} alt="logo"></img>
+          {/* <img src={logo2} alt="logo"></img> */}
           <h3>너만의 전시회</h3>
         </StyledLogo>
       </Link>
@@ -31,9 +51,10 @@ function Header() {
 
 const StyledHeader = styled.header`
   display: flex;
+  position: fixed;
   justify-content: space-between;
   width: 100%;
-  margin: 15px 0 33px;
+  margin: 34px 0 33px;
 `;
 
 const StyledLogo = styled.div`
@@ -51,6 +72,7 @@ const StyledLogo = styled.div`
     font-size: 24px;
     font-weight: 600;
     text-align: center;
+    color: white;
   }
 `;
 
@@ -69,6 +91,7 @@ const HeaderRight = styled.div`
     border-radius: 7px;
     padding: 10px;
     margin-right: 24px;
+    cursor: pointer;
   }
   img {
     width: 28px;
