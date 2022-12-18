2eea9cda39c3c7948a612f0b0fe82810230eb5d6
thyeone <thyeonee@gmail.com>
2022-12-18 19:31:32 +0900

    refactor: alert 문구 변경

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -20,8 +20,7 @@ function Login() {
         navigate("/main");
       })
       .catch((error) => {
-        alert("로그인 실패");
-        console.log("로그인 실패", error);
+        alert("아이디 또는 비밀번호를 확인해주세요.");
       });
   };
   useEffect(() => {
